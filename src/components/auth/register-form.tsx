a2e182b363fd8a40c9e79432858f2e75cf69afef
Oscar Bertel P <osber00@gmail.com>
2025-04-24 23:20:05 -0500

    Proyecto Base NextJs

---
+++ src/components/auth/register-form.tsx
@@ -0,0 +1,64 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import Link from "next/link"
+
+export function RegisterForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader className="text-center">
+          <CardTitle className="text-xl">Registrarse</CardTitle>
+          
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid gap-6">
+              
+              <div className="grid gap-6">
+                <div className="grid gap-3">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    placeholder="usuario@ejemplo.com"
+                    required
+                  />
+                </div>
+                <div className="grid gap-3">
+                  <div className="flex items-center">
+                    <Label htmlFor="password">Password</Label>
+                    
+                  </div>
+                  <Input id="password" type="password" placeholder="*********" required />
+                </div>
+                <Button type="submit" className="w-full cursor-pointer">
+                  Registrarse
+                </Button>
+              </div>
+              <div className="text-center text-sm">
+                ¿Tienes cuenta?{" "}
+                <Link href="/login" className="underline underline-offset-4">Inicia sesión</Link>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+      {/* <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+        and <a href="#">Privacy Policy</a>.
+      </div> */}
+    </div>
+  )
+}
